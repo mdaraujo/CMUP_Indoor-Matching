34470fee2f9975cab1c8cdfd01b10edba2cabcb3
unknown <mdevelap@gmail.com>
2019-05-29 00:14:07 +0100

    sharing code between main activities with BaseMainActivity

--- IndoorMatching/app/src/main/java/com/mdaraujo/indoormatching/MainActivity.java
+++ IndoorMatching/app/src/main/java/com/mdaraujo/indoormatching/MainActivity.java
@@ -1,14 +1,7 @@
 package com.mdaraujo.indoormatching;
 
-import android.Manifest;
-import android.app.AlertDialog;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
-import android.os.RemoteException;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -24,21 +17,15 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.mdaraujo.commonlibrary.RoomCanvasView;
+import com.mdaraujo.commonlibrary.BaseMainActivity;
 import com.mdaraujo.commonlibrary.model.BeaconInfo;
 import com.mdaraujo.commonlibrary.model.Room;
 
 import org.altbeacon.beacon.Beacon;
-import org.altbeacon.beacon.BeaconConsumer;
-import org.altbeacon.beacon.BeaconManager;
-import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
-import org.altbeacon.beacon.service.ArmaRssiFilter;
-import org.altbeacon.beacon.service.RunningAverageRssiFilter;
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
@@ -51,20 +38,12 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import static com.mdaraujo.commonlibrary.CommonParams.PERMISSION_REQUEST_COARSE_LOCATION;
 import static com.mdaraujo.commonlibrary.model.BeaconInfo.BEACONS_COLLECTION_NAME;
 import static com.mdaraujo.commonlibrary.model.Room.ROOMS_COLLECTION_NAME;
 
-public class MainActivity extends AppCompatActivity implements BeaconConsumer, RangeNotifier {
+public class MainActivity extends BaseMainActivity implements RangeNotifier {
 
     private static String TAG = "MainActivity";
-
-    private RoomCanvasView roomCanvas;
-
-    private BeaconManager mBeaconManager;
-    private FirebaseFirestore firestoreDb;
-    private Room room;
-    private List<BeaconInfo> beaconsInfo;
 
     private TextView roomNameView;
 
@@ -89,36 +68,9 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
             finish();
         }
 
-        firestoreDb = FirebaseFirestore.getInstance();
-
-//        BeaconManager.setRssiFilterImplClass(RunningAverageRssiFilter.class);
-//        RunningAverageRssiFilter.setSampleExpirationMilliseconds(3000L);
-
-        BeaconManager.setRssiFilterImplClass(ArmaRssiFilter.class);
-//        ArmaRssiFilter.setDEFAULT_ARMA_SPEED(0.1);
-
-        room = null;
-        beaconsInfo = new ArrayList<>();
-
         roomNameView = findViewById(R.id.room_name_text);
         roomCanvas = findViewById(R.id.room_canvas);
 
-        verifyBluetooth();
-
-        Log.d(TAG, "onCreate");
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // Android M Permission check
-            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("This app needs location access");
-                builder.setMessage("Please grant location access so this app can detect beacons in the background.");
-                builder.setPositiveButton(android.R.string.ok, null);
-                builder.setOnDismissListener(dialog -> requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        PERMISSION_REQUEST_COARSE_LOCATION));
-                builder.show();
-            }
-        }
 
         String clientId = MqttClient.generateClientId();
         client = new MqttAndroidClient(this.getApplicationContext(), "tcp://192.168.43.100:1883",
@@ -160,38 +112,8 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-
-        refreshScan();
-    }
-
-    private void refreshScan() {
-        room = null;
-        beaconsInfo = new ArrayList<>();
-
-        mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
-        mBeaconManager.getBeaconParsers().add(new BeaconParser().
-                setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
-
-//        // set the duration of the scan to be 1.1 seconds
-//        mBeaconManager.setBackgroundScanPeriod(1100l);
-//        // set the time between each scan to be 1 hour (3600 seconds)
-//        mBeaconManager.setBackgroundBetweenScanPeriod(3600000l);
-
-        mBeaconManager.setForegroundScanPeriod(300L);
-        mBeaconManager.setForegroundBetweenScanPeriod(0L); // duration spent not scanning between each Bluetooth scan cycle
-
-        mBeaconManager.bind(this);
-    }
-
     public void onBeaconServiceConnect() {
-        Region region = new Region("all-beacons-region", null, null, null);
-        try {
-            mBeaconManager.startRangingBeaconsInRegion(region);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
+        super.onBeaconServiceConnect();
         mBeaconManager.addRangeNotifier(this);
     }
 
@@ -235,55 +157,6 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
                 beaconsToDraw.add(beaconInfo);
 
         roomCanvas.drawBeacons(beaconsToDraw);
-    }
-
-    private BeaconInfo getBeaconFromList(String instanceId) {
-        for (BeaconInfo beacon : beaconsInfo) {
-            if (beacon.getInstanceId().equals(instanceId))
-                return beacon;
-        }
-        return null;
-    }
-
-    private void addFoundBeaconToList(Beacon foundBeacon) {
-        beaconsInfo.add(new BeaconInfo(foundBeacon.getId1().toHexString(),
-                foundBeacon.getId2().toHexString(), foundBeacon.getBluetoothAddress(),
-                foundBeacon.getRssi(), foundBeacon.getDistance(), true));
-    }
-
-    private void getBeaconsOfRoom(String roomKey) {
-        Query queryBeacons = firestoreDb.collection(BEACONS_COLLECTION_NAME)
-                .whereEqualTo("roomKey", roomKey);
-
-        queryBeacons.get()
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                    @Override
-                    public void onSuccess(QuerySnapshot documentSnapshots) {
-
-                        if (!documentSnapshots.isEmpty()) {
-
-                            for (DocumentSnapshot beaconSnapshot : documentSnapshots.getDocuments()) {
-                                BeaconInfo retrievedBeacon = beaconSnapshot.toObject(BeaconInfo.class);
-
-                                BeaconInfo beaconFound = getBeaconFromList(retrievedBeacon.getInstanceId());
-
-                                if (beaconFound != null) {
-                                    beaconFound.setRoomKey(roomKey);
-                                    beaconFound.setPosX(retrievedBeacon.getPosX());
-                                    beaconFound.setPosY(retrievedBeacon.getPosY());
-                                    beaconFound.setName(retrievedBeacon.getName());
-                                    beaconFound.setColor(retrievedBeacon.getColor());
-                                    Log.i(TAG, "getBeaconsOfRoom: beaconFound.setRoomKey(roomKey) " + beaconFound.getInstanceId());
-                                } else {
-                                    beaconsInfo.add(retrievedBeacon);
-                                    Log.i(TAG, "getBeaconsOfRoom: beaconsInfo.add() " + retrievedBeacon.getInstanceId());
-                                }
-                            }
-                        } else {
-                            Log.i(TAG, "Zero beacons associated with room " + roomKey);
-                        }
-                    }
-                });
     }
 
     private void getRoomOfBeacon(Beacon foundBeacon) {
@@ -334,58 +207,6 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
                 });
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        mBeaconManager.unbind(this);
-    }
-
-    private void verifyBluetooth() {
-        try {
-            if (!BeaconManager.getInstanceForApplication(this).checkAvailability()) {
-                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Bluetooth not enabled");
-                builder.setMessage("Please enable bluetooth in settings and restart this application.");
-                builder.setPositiveButton(android.R.string.ok, null);
-                builder.setOnDismissListener(dialog -> {
-                    finish();
-                    System.exit(0);
-                });
-                builder.show();
-            }
-        } catch (RuntimeException e) {
-            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Bluetooth LE not available");
-            builder.setMessage("Sorry, this device does not support Bluetooth LE.");
-            builder.setPositiveButton(android.R.string.ok, null);
-            builder.setOnDismissListener(dialog -> {
-                finish();
-                System.exit(0);
-            });
-            builder.show();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[],
-                                           @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_REQUEST_COARSE_LOCATION: {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d(TAG, "coarse location permission granted");
-                } else {
-                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    builder.setTitle("Functionality limited");
-                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
-                    builder.setPositiveButton(android.R.string.ok, null);
-                    builder.setOnDismissListener(dialog -> {
-                    });
-                    builder.show();
-                }
-            }
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- IndoorMatching/indoorconfig/src/main/java/com/mdaraujo/indoorconfig/MainActivity.java
+++ IndoorMatching/indoorconfig/src/main/java/com/mdaraujo/indoorconfig/MainActivity.java
@@ -1,15 +1,8 @@
 package com.mdaraujo.indoorconfig;
 
-import android.Manifest;
-import android.app.AlertDialog;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.RemoteException;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -29,43 +22,30 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.mdaraujo.commonlibrary.RoomCanvasView;
+import com.mdaraujo.commonlibrary.BaseMainActivity;
 import com.mdaraujo.commonlibrary.model.BeaconInfo;
 import com.mdaraujo.commonlibrary.model.Room;
 
 import org.altbeacon.beacon.Beacon;
-import org.altbeacon.beacon.BeaconConsumer;
-import org.altbeacon.beacon.BeaconManager;
-import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
-import org.altbeacon.beacon.service.ArmaRssiFilter;
-import org.altbeacon.beacon.service.RunningAverageRssiFilter;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-import static com.mdaraujo.commonlibrary.CommonParams.PERMISSION_REQUEST_COARSE_LOCATION;
 import static com.mdaraujo.commonlibrary.model.BeaconInfo.BEACONS_COLLECTION_NAME;
 import static com.mdaraujo.commonlibrary.model.Room.ROOMS_COLLECTION_NAME;
 
-public class MainActivity extends AppCompatActivity implements BeaconConsumer, RangeNotifier, RecyclerViewClickListener {
+public class MainActivity extends BaseMainActivity implements RangeNotifier, RecyclerViewClickListener {
 
     private static String TAG = "MainActivity";
 
-    private BeaconManager mBeaconManager;
-
-    private RoomCanvasView roomCanvas;
-
-    private FirebaseFirestore firestoreDb;
-    private Room room;
     private String roomKey;
-    private List<BeaconInfo> beaconsInfo;
+
     private BeaconsAdapter beaconsAdapter;
     private RecyclerView recyclerView;
 
@@ -94,14 +74,6 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
             finish();
         }
 
-        firestoreDb = FirebaseFirestore.getInstance();
-
-        BeaconManager.setRssiFilterImplClass(RunningAverageRssiFilter.class);
-        RunningAverageRssiFilter.setSampleExpirationMilliseconds(8000L);
-
-//        BeaconManager.setRssiFilterImplClass(ArmaRssiFilter.class);
-//        ArmaRssiFilter.setDEFAULT_ARMA_SPEED(0.3);
-
         recyclerView = (RecyclerView) findViewById(R.id.beacons_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setNestedScrollingEnabled(false);
@@ -112,55 +84,18 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
 
         roomCanvas = findViewById(R.id.room_canvas);
 
-        verifyBluetooth();
-
-        Log.i(TAG, "onCreate");
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // Android M Permission check
-            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("This app needs location access");
-                builder.setMessage("Please grant location access so this app can detect beacons in the background.");
-                builder.setPositiveButton(android.R.string.ok, null);
-                builder.setOnDismissListener(dialog -> requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        PERMISSION_REQUEST_COARSE_LOCATION));
-                builder.show();
-            }
-        }
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-
-        room = null;
-        beaconsInfo = new ArrayList<>();
+    protected void refreshScan() {
+        super.refreshScan();
         beaconsAdapter = new BeaconsAdapter(beaconsInfo, this);
         recyclerView.setAdapter(beaconsAdapter);
-
-        mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
-        mBeaconManager.getBeaconParsers().add(new BeaconParser().
-                setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
-
-//        // set the duration of the scan to be 1.1 seconds
-//        mBeaconManager.setBackgroundScanPeriod(1100l);
-//        // set the time between each scan to be 1 hour (3600 seconds)
-//        mBeaconManager.setBackgroundBetweenScanPeriod(3600000l);
-
-        mBeaconManager.setForegroundScanPeriod(1000L);
-        mBeaconManager.setForegroundBetweenScanPeriod(0L); // duration spent not scanning between each Bluetooth scan cycle
-
-        mBeaconManager.bind(this);
     }
 
+    @Override
     public void onBeaconServiceConnect() {
-        Region region = new Region("all-beacons-region", null, null, null);
-        try {
-            mBeaconManager.startRangingBeaconsInRegion(region);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
+        super.onBeaconServiceConnect();
         mBeaconManager.addRangeNotifier(this);
     }
 
@@ -226,55 +161,6 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
         startActivity(beaconConfigIntent);
     }
 
-    private BeaconInfo getBeaconFromList(String instanceId) {
-        for (BeaconInfo beacon : beaconsInfo) {
-            if (beacon.getInstanceId().equals(instanceId))
-                return beacon;
-        }
-        return null;
-    }
-
-    private void addFoundBeaconToList(Beacon foundBeacon) {
-        beaconsInfo.add(new BeaconInfo(foundBeacon.getId1().toHexString(),
-                foundBeacon.getId2().toHexString(), foundBeacon.getBluetoothAddress(),
-                foundBeacon.getRssi(), foundBeacon.getDistance(), true));
-    }
-
-    private void getBeaconsOfRoom(String roomKey) {
-        Query queryBeacons = firestoreDb.collection(BEACONS_COLLECTION_NAME)
-                .whereEqualTo("roomKey", roomKey);
-
-        queryBeacons.get()
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                    @Override
-                    public void onSuccess(QuerySnapshot documentSnapshots) {
-
-                        if (!documentSnapshots.isEmpty()) {
-
-                            for (DocumentSnapshot beaconSnapshot : documentSnapshots.getDocuments()) {
-                                BeaconInfo retrievedBeacon = beaconSnapshot.toObject(BeaconInfo.class);
-
-                                BeaconInfo beaconFound = getBeaconFromList(retrievedBeacon.getInstanceId());
-
-                                if (beaconFound != null) {
-                                    beaconFound.setRoomKey(roomKey);
-                                    beaconFound.setPosX(retrievedBeacon.getPosX());
-                                    beaconFound.setPosY(retrievedBeacon.getPosY());
-                                    beaconFound.setName(retrievedBeacon.getName());
-                                    beaconFound.setColor(retrievedBeacon.getColor());
-                                    Log.i(TAG, "getBeaconsOfRoom: beaconFound.setRoomKey(roomKey) " + beaconFound.getInstanceId());
-                                } else {
-                                    beaconsInfo.add(retrievedBeacon);
-                                    Log.i(TAG, "getBeaconsOfRoom: beaconsInfo.add() " + retrievedBeacon.getInstanceId());
-                                }
-                            }
-                        } else {
-                            Log.i(TAG, "Zero beacons associated with room " + roomKey);
-                        }
-                    }
-                });
-    }
-
     private void getRoomOfBeacon(Beacon foundBeacon) {
         String instanceId = foundBeacon.getId2().toHexString();
 
@@ -325,59 +211,6 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
                         }
                     }
                 });
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        mBeaconManager.unbind(this);
-    }
-
-    private void verifyBluetooth() {
-        try {
-            if (!BeaconManager.getInstanceForApplication(this).checkAvailability()) {
-                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Bluetooth not enabled");
-                builder.setMessage("Please enable bluetooth in settings and restart this application.");
-                builder.setPositiveButton(android.R.string.ok, null);
-                builder.setOnDismissListener(dialog -> {
-                    finish();
-                    System.exit(0);
-                });
-                builder.show();
-            }
-        } catch (RuntimeException e) {
-            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Bluetooth LE not available");
-            builder.setMessage("Sorry, this device does not support Bluetooth LE.");
-            builder.setPositiveButton(android.R.string.ok, null);
-            builder.setOnDismissListener(dialog -> {
-                finish();
-                System.exit(0);
-            });
-            builder.show();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[],
-                                           @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_REQUEST_COARSE_LOCATION: {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d(TAG, "coarse location permission granted");
-                } else {
-                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    builder.setTitle("Functionality limited");
-                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
-                    builder.setPositiveButton(android.R.string.ok, null);
-                    builder.setOnDismissListener(dialog -> {
-                    });
-                    builder.show();
-                }
-            }
-        }
     }
 
     public void addNewRoomBtnClick(View view) {
@@ -439,7 +272,7 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
                 if (mBeaconManager.isBound(this)) {
                     mBeaconManager.unbind(this);
                 }
-                mBeaconManager.bind(this);
+                refreshScan();
             default:
                 return super.onOptionsItemSelected(item);
 
