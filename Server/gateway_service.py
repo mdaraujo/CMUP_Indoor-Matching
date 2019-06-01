902594aec87aa567213dc539effdabf26b8e87bf
unknown <mdevelap@gmail.com>
2019-06-02 00:23:08 +0100

    gateway service checking user inactivity

--- Server/gateway_service.py
+++ Server/gateway_service.py
@@ -1,9 +1,11 @@
 import json
+from datetime import datetime
 import paho.mqtt.client as mqtt
 
 
 BROKER_ADDRESS = "localhost"
 GATEWAY_TOPIC = "gateway_service"
+MAX_INACTIVITY_TIME = 5
 
 client = mqtt.Client()
 
@@ -18,14 +20,11 @@ class User:
         self.y = None
         self.match_id = None
         self.interests = None
-        # TODO add last timestamp to delete not responding users
+        self.timestamp = datetime.now()
 
     def setPos(self, x, y):
         self.x = x
         self.y = y
-
-    def setMatch(self, match_id):
-        self.match_id = match_id
 
     def to_json(self):
         """ Convert to a json object. """
@@ -41,9 +40,39 @@ class User:
         return "User ID: {}, Name: {}, X: {}, Y: {}, Match ID: {}".format(self.uid, self.name, self.x, self.y, self.match_id)
 
 
-def printUsers():
+def print_users():
+    print("Users List:")
     for uid, user in users.items():
         print(user)
+
+
+def checkUserInactivity(user_id):
+    td = datetime.now() - users[user_id].timestamp
+
+    if td.total_seconds() > MAX_INACTIVITY_TIME:
+        del users[user_id]
+        return True
+    return False
+
+
+def matchUser(user_id):
+    for uid, other_user in users.items():
+        if uid != user_id and other_user.match_id is None:
+            if checkUserInactivity(other_user.uid):
+                continue
+
+            users[user_id].match_id = uid
+            other_user.match_id = user_id
+            sendInfoToMatch(users[user_id])
+
+
+def sendInfoToMatch(user):
+    match_topic = "users/" + user.match_id
+    json_msg = user.to_json()
+    print("Publishing message to topic",
+          match_topic, json_msg, sep=" : ")
+    print()
+    client.publish(match_topic, json_msg)
 
 
 def on_connect(client, userdata, flags, rc):
@@ -63,30 +92,36 @@ def on_message(client, userdata, msg):
     user_id = json_msg['userId']
 
     if msg_type == 0:                           # update user info
-        users[user_id] = User(user_id, json_msg['name'])
+        user = users.get(user_id, None)
+        if user:
+            user.x = None
+            user.y = None
+            # user.interests = json_msg['interests']
+            user.name = json_msg['name']
+            user.timestamp = datetime.now()
+        else:
+            users[user_id] = User(user_id, json_msg['name'])
 
     elif msg_type == 1:                         # update user position
         user = users.get(user_id, None)
         if user:
             user.setPos(json_msg['x'], json_msg['y'])
+            user.timestamp = datetime.now()
             # check match
             if user.match_id is not None:
-                # send new info to match
-                match_topic = "users/" + user.match_id
-                json_msg = users[user_id].to_json()
-                print("Publishing message to topic",
-                      match_topic, json_msg, sep=" : ")
-                print()
-                client.publish(match_topic, json_msg)
+                # check match timestamp
+                if checkUserInactivity(user.match_id):
+                    user.match_id = None
+                    matchUser(user_id)
+                else:
+                    # send new info to match
+                    sendInfoToMatch(user)
 
             else:
                 # match user with similar available user
-                for uid, other_user in users.items():
-                    if uid != user_id and other_user.match_id is None:
-                        user.match_id = uid
-                        other_user.match_id = user_id
+                matchUser(user_id)
 
-    printUsers()
+    print_users()
 
 
 def main():
